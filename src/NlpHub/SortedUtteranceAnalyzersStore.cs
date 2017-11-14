21f4b995f5df24e990110f557efe6ac2418064e1
Fabio Maulo <fabiomaulo@gmail.com>
2017-11-14 00:28:43 -0300

    SortedUtteranceAnalyzersStore

---
+++ src/NlpHub/SortedUtteranceAnalyzersStore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace NlpHub
+{
+	public class SortedUtteranceAnalyzersStore : IUtteranceAnalyzersStore
+	{
+		private List<IUtteranceAnalyzer> analyzers = new List<IUtteranceAnalyzer>();
+
+		public IUtteranceAnalyzersStore Register(IUtteranceAnalyzer analyzer)
+		{
+			if(analyzer != null)
+			{
+				analyzers.Add(analyzer);
+			}
+			return this;
+		}
+
+		public IEnumerable<IUtteranceAnalyzer> Registered() => analyzers;
+	}
+}
---
+++ test/NplHubTests/SortedUtteranceAnalyzersStoreTests/AddingAnalyzersTests.cs
@@ -0,0 +1,63 @@
+﻿using NlpHub;
+using NUnit.Framework;
+using SharpTestsEx;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NplHubTests.SortedUtteranceAnalyzersStoreTests
+{
+	public class AddingAnalyzersTests
+	{
+		public class Fake1 : IUtteranceAnalyzer
+		{
+			public Task<IEnumerable<AnalyzedResult>> Analyze(string utterance)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public class Fake2 : IUtteranceAnalyzer
+		{
+			public Task<IEnumerable<AnalyzedResult>> Analyze(string utterance)
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		[Test]
+		public void WhenNoregistrationThenEmpty()
+		{
+			IUtteranceAnalyzersStore analyzers = new SortedUtteranceAnalyzersStore();
+			IEnumerable<IUtteranceAnalyzer> actual = analyzers.Registered();
+			actual.Should().Be.Empty();
+		}
+
+		[Test]
+		public void WhenAddNullThenEmpty()
+		{
+			IUtteranceAnalyzersStore analyzers = new SortedUtteranceAnalyzersStore();
+			analyzers.Register(null);
+			analyzers.Registered().Should().Be.Empty();
+		}
+
+		[Test]
+		public void WhenAddOneThenHaveOne()
+		{
+			IUtteranceAnalyzersStore analyzers = new SortedUtteranceAnalyzersStore();
+			analyzers.Register(new Fake1());
+			analyzers.Registered().Should().Have.Count.EqualTo(1);
+		}
+
+		[Test]
+		public void WhenAddTwoThenReturnInSequence()
+		{
+			IUtteranceAnalyzersStore analyzers = new SortedUtteranceAnalyzersStore();
+			var a1 = new Fake1();
+			var a2 = new Fake2();
+			analyzers.Register(a1);
+			analyzers.Register(a2);
+			analyzers.Registered().Should().Have.SameSequenceAs(a1, a2);
+		}
+	}
+}
