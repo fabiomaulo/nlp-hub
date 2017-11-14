da9bf6bbf90a4abe3fd24ce999f33227bfdea47f
Fabio Maulo <fabiomaulo@gmail.com>
2017-11-13 21:18:16 -0300

    Failing tests (but discovered with funky!)

---
+++ test/NplHubTests/AnalyzedResultTests/PropertiesGettersTests.cs
@@ -0,0 +1,39 @@
+﻿using NlpHub;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace NplHubTests.AnalyzedResultTests
+{
+	public class PropertiesGettersTests
+	{
+		[Test]
+		public void WhenCreateThenAnalyzerIdIsNotNull()
+		{
+			var actual = new AnalyzedResult();
+			actual.AnalyzerId.Should().Not.Be.Null();
+		}
+
+		[Test]
+		public void WhenAssignNullToAnalyzerIdThenAnalyzerIdIsNotNull()
+		{
+			var actual = new AnalyzedResult();
+			actual.AnalyzerId = null;
+			actual.AnalyzerId.Should().Not.Be.Null();
+		}
+
+		[Test]
+		public void WhenCreateThenEntitiesIsNotNull()
+		{
+			var actual = new AnalyzedResult();
+			actual.Entities.Should().Not.Be.Null();
+		}
+
+		[Test]
+		public void WhenAssignNullToEntitiesThenEntitiesIsEmpty()
+		{
+			var actual = new AnalyzedResult();
+			actual.Entities = null;
+			actual.Entities.Should().Not.Be.Null();
+		}
+	}
+}
