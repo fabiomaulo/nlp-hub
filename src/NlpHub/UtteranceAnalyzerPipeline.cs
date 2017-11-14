b25794c9e098d474585a6db4dc7a2e726ca08d3d
Fabio Maulo <fabiomaulo@gmail.com>
2017-11-14 06:39:14 -0300

    Pipeline with examples

---
+++ src/NlpHub/UtteranceAnalyzerPipeline.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NlpHub
+{
+	/// <summary>
+	/// Templete class to create a pipeline of analyzers
+	/// </summary>
+	public abstract class UtteranceAnalyzerPipeline : IUtteranceAnalyzer
+	{
+		private readonly IUtteranceAnalyzer innerAnalyzer;
+
+		protected UtteranceAnalyzerPipeline()
+			:this(new NoOpUtteranceAnalyzer())
+		{
+		}
+
+		protected UtteranceAnalyzerPipeline(IUtteranceAnalyzer innerAnalyzer)
+		{
+			this.innerAnalyzer = innerAnalyzer ?? throw new ArgumentNullException(nameof(innerAnalyzer));
+		}
+
+		protected IUtteranceAnalyzer InnerAnalyzer => innerAnalyzer;
+
+		public abstract Task<IEnumerable<AnalyzedResult>> Analyze(string utterance);
+	}
+
+	/// <summary>
+	/// Do nothing analyzer to use as the end of the pipeline
+	/// </summary>
+	public class NoOpUtteranceAnalyzer : IUtteranceAnalyzer
+	{
+		private static Task<IEnumerable<AnalyzedResult>> empty = Task.FromResult<IEnumerable<AnalyzedResult>>(new AnalyzedResult[0]);
+		public Task<IEnumerable<AnalyzedResult>> Analyze(string utterance) => empty;
+	}
+}
---
+++ test/NplHubTests/UtteranceAnalyzerPipelineExample/Example01.cs
@@ -0,0 +1,41 @@
+﻿using NlpHub;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NplHubTests.UtteranceAnalyzerPipelineTests
+{
+	public class Example01
+	{
+		internal class FirstInPipeline : UtteranceAnalyzerPipeline
+		{
+			protected FirstInPipeline(IUtteranceAnalyzer innerAnalyzer) : base(innerAnalyzer)
+			{
+				// required inner
+			}
+
+			public async override Task<IEnumerable<AnalyzedResult>> Analyze(string utterance)
+			{
+				// Analyze first and then analyze the inner
+				var analyzed = new[] { new AnalyzedResult() };
+				return analyzed.Concat(await InnerAnalyzer.Analyze(utterance));
+			}
+		}
+
+		internal class SecondInPipeline : UtteranceAnalyzerPipeline
+		{
+			public SecondInPipeline(IUtteranceAnalyzer innerAnalyzer = null) : base(innerAnalyzer ?? new NoOpUtteranceAnalyzer())
+			{
+				// optional inner
+			}
+
+			public async override Task<IEnumerable<AnalyzedResult>> Analyze(string utterance)
+			{
+				// Analyze the inner first and then analyze this
+				var analyzedByInner = await InnerAnalyzer.Analyze(utterance);
+				var analyzed = new[] { new AnalyzedResult() };
+				return analyzedByInner.Concat(analyzed);
+			}
+		}
+	}
+}
